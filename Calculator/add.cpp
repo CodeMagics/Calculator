96bec4c85b34a568f810e434a0e8ee13276f09c1
20131302 <643087116@qq.com>
2015-07-14 20:04:53 +0800

    完善了除法，乘法

--- Calculator/add.cpp
+++ Calculator/add.cpp
@@ -1,6 +1,22 @@
+#include "Add.h"
 int Add(int num1, int num2)
 {
 
 
 	return num1 + num2;
+}
+
+CAdd::CAdd()
+{
+}
+
+
+CAdd::~CAdd()
+{
+}
+
+
+int CAdd::operation()
+{
+	return num1-num2;
 }
--- Calculator/main.cpp
+++ Calculator/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 using namespace std;
+#include"Factory.h"
+#include"Operation.h"
 
 //程序的入口
 int main()
@@ -10,6 +12,13 @@ int main()
 	cin >> a;
 	cin >> op;
 	cin >> b;
+	CFactory *factory = new CFactory();
+	COperation* operation = factory->init(op);
+	operation->num1 = a;
+	operation->num2 = b;
+	cout << a << op << b << "=" << operation->operation() << endl;
+
+
 	
 	
 }
